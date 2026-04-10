b3dc68fac72701419794f5d5590e552a22c4643d
pinguduck <vodit@aol.com>
2026-04-10 21:34:06 +0100

    Update script.js
    anoter test

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ async function listZones() {
 }
 async function fetchPopularity() {
     try {
-        const response = await fetch("https://data.jsdelivr.com/v1/stats/packages/gh/gn-math/html@main/files?period=year");
+        const response = await fetch("./zones.json");
         const data = await response.json();
         data.forEach(file => {
             const idMatch = file.name.match(/\/(\d+)\.html$/);
