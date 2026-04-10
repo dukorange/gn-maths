807c8b1d420157a24add92f4f854ee1b9a4a14c9
pinguduck <vodit@aol.com>
2026-04-10 21:46:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ async function listZones() {
 }
 async function fetchPopularity() {
     try {
-        const response = await fetch("./zones.json");
+        const response = await fetch("zones.json");
         const data = await response.json();
         data.forEach(file => {
             const idMatch = file.name.match(/\/(\d+)\.html$/);
